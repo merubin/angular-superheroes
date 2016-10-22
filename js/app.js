4ee62a503f5edeb9182a8f1615093a4f48363917
Michael Rubin <michael@rubinsworld.com>
2016-10-22 13:27:43 -0400

    moved files to folders

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 superheroPhonebook = [
-  { name: 'Spider-Man', affiliation : "Marvel", phoneNumber: 9159687825, photo_url: "http://www.pngall.com/wp-content/uploads/2016/05/Spider-Man-PNG-Pic.png"},
-  { name: 'Superman', affiliation : "DC", phoneNumber: 5309384645, photo_url: "http://www.planwallpaper.com/static/images/Superman1.jpg"},
-  { name: 'Atlas', affiliation : "Lone Star", phoneNumber: 8124242222, photo_url: "http://www.idwpublishing.com/wp-content/uploads/2014/08/ICO002716_21.jpg"},
-  { name: 'Blacklight', affiliation : "Image", phoneNumber: 8124242222, photo_url: "https://upload.wikimedia.org/wikipedia/en/8/87/Blacklight_01_cover.jpg"}
+  { name: 'Spider-Man', affiliation : "Marvel", phoneNumber: 9159687825, photo_url: "images/Spider-Man.png"},
+  { name: 'Superman', affiliation : "DC", phoneNumber: 5309384645, photo_url: "images/Superman.jpg"},
+  { name: 'Atlas', affiliation : "Lone Star", phoneNumber: 8124242222, photo_url: "images/atlas.jpg"},
+  { name: 'Blacklight', affiliation : "Image", phoneNumber: 8124242222, photo_url: "images/blacklight.jpg"},
+  { name: 'Wonder Woman', affiliation : "DC", phoneNumber: 2036917716, photo_url: "images/wonder-woman.jpg"}
+
+
 ]
 //  setter
 angular
