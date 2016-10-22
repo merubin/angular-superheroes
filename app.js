03a3ecc9f5073fe437323cb1db88335843aa8d04
Michael Rubin <michael@rubinsworld.com>
2016-10-22 08:54:38 -0400

    Search box is filtering on phonebook

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 superheroPhonebook = [
-  { name: 'Spider-Man', affiliation : "Marvel", phoneNumber: "9159687825", photo_url: "http://www.pngall.com/wp-content/uploads/2016/05/Spider-Man-PNG-Pic.png"},
-  { name: 'Superman', affiliation : "DC", phoneNumber: "5309384645", photo_url: "http://www.planwallpaper.com/static/images/Superman1.jpg"},
-  { name: 'Atlas', affiliation : "Lone Star", phoneNumber: "8124242222", photo_url: "http://www.idwpublishing.com/wp-content/uploads/2014/08/ICO002716_21.jpg"},
-  { name: 'Blacklight', affiliation : "Image", phoneNumber: "8124242222", photo_url: "https://upload.wikimedia.org/wikipedia/en/8/87/Blacklight_01_cover.jpg"}
+  { name: 'Spider-Man', affiliation : "Marvel", phoneNumber: 9159687825, photo_url: "http://www.pngall.com/wp-content/uploads/2016/05/Spider-Man-PNG-Pic.png"},
+  { name: 'Superman', affiliation : "DC", phoneNumber: 5309384645, photo_url: "http://www.planwallpaper.com/static/images/Superman1.jpg"},
+  { name: 'Atlas', affiliation : "Lone Star", phoneNumber: 8124242222, photo_url: "http://www.idwpublishing.com/wp-content/uploads/2014/08/ICO002716_21.jpg"},
+  { name: 'Blacklight', affiliation : "Image", phoneNumber: 8124242222, photo_url: "https://upload.wikimedia.org/wikipedia/en/8/87/Blacklight_01_cover.jpg"}
 ]
 //  setter
 angular
 .module('superherosApp', [])
 .controller("superherosCtrl", [ superherosController ])
 
+.filter("phone",function() {
+   return function (phoneNumber) {
+  number = String(phoneNumber);
+  return("("+number.substring(0,3)+") "+number.substring(3,6)+"-"+number.substring(6,10))
+}})
+
+
+
 function superherosController () {
   this.superheros = superheroPhonebook
   this.newSuperhero={}   /* initial new todo */
+  this.lastSearchTerm=""
   this.addSuperhero = function () {
 
     this.newSuperhero={ name:this.newSuperhero.name, affiliation:this.newSuperhero.affiliation,
@@ -21,4 +30,9 @@ function superherosController () {
     this.newSuperhero={ name: '', affiliation: '',
                      phoneNumber:'', photo_url: '' }
   }
+  this.searchSuperhero= function(){
+   console.log('Search Pressed'+this.search)
+   this.lastSearchTerm=this.search
+   document.getElementById("SearchButton").value = "";
+  };
 }
